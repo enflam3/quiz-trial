bb64cd145d2ebafbfc9278456ff156a255f466a1
Emils <47244576+enflam3@users.noreply.github.com>
2019-02-28 09:22:57 +0200

    Update config.php
    UTF-8 support added

--- php/config.php
+++ php/config.php
@@ -9,7 +9,7 @@ $dbname = "XXX";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 //Charset UTF-8
-mysqli_set_charset('utf8', $conn);
+mysqli_set_charset($conn,"utf8");
 
 // Check connection
 if ($conn->connect_error) {
